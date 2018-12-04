819d704ac0358aa3d1e8a9ba0a0d0c32f26cfae3
Parvez Nawaz <ParvezN@gs1ca.org>
2018-12-04 16:22:29 -0500

    snackbar added

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -5,13 +5,19 @@ import { Injectable, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { TrainingService } from '../training/training.service';
+import { MatSnackBar } from '@angular/material';
 
 @Injectable()
 export class AuthService implements OnInit {
   authChange = new Subject<boolean>();
   private isAuthenticated: boolean = false;
 
-  constructor(private router: Router, private afAuth: AngularFireAuth, private traininigService: TrainingService){}
+  constructor(
+    private router: Router, 
+    private afAuth: AngularFireAuth, 
+    private traininigService: TrainingService,
+    private snackBar: MatSnackBar
+  ){}
 
   ngOnInit(): void {}
 
@@ -32,7 +38,14 @@ export class AuthService implements OnInit {
 
   registerUser(authData: AuthData) {
     this.afAuth.auth
-      .createUserWithEmailAndPassword(authData.email,authData.password);
+      .createUserWithEmailAndPassword(authData.email,authData.password)
+      .then()
+      .catch(error=>{
+        this.snackBar.open(error.message, null, 
+          {
+            duration: 3000
+          })
+      });
   }
 
   login(authData: AuthData) {
